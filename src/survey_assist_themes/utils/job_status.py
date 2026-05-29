cbbf2422fc7c55f27f7f4f03c575beb5dbe5f00e
Ethan Moss <ethan.moss@ons.gov.uk>
2026-05-29 13:48:47 +0100

    feat: added initial db connection

--- src/survey_assist_themes/utils/job_status.py
+++ src/survey_assist_themes/utils/job_status.py
@@ -1,8 +1,35 @@
 """Handler for recording job statuses in a firestore db."""
 
+from firebase_admin import firestore, initialize_app
+
 
 class JobStatus:
     """Connect to a firestore db and create/update the status of a job."""
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        gcp_project_id: str,
+        firestore_db_name: str,
+        timeout: int | float = 5,
+    ):
+        self.gcp_project_id = gcp_project_id
+        self.firestore_db_name = firestore_db_name
+        self.timeout = timeout
+
+        # Initialize Firestore connection
+        app_options = {
+            "projectId": self.gcp_project_id,
+            "httpTimeout": self.timeout,
+        }
+        app = initialize_app(options=app_options)
+        self._db = firestore.client(
+            app=app, database_id=self.firestore_db_name
+        )
+
+        # non-intrusive test to verify db connection - list all collections
+        try:
+            _ = list(self._db.collections())
+        except Exception as e:
+            raise ConnectionError(
+                f"Error when connecting to Firestore: {e}"
+            ) from e
