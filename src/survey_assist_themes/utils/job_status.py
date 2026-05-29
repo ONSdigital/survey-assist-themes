d3d520a2ef631f94eb969081b4366c06afa53b15
Ethan Moss <ethan.moss@ons.gov.uk>
2026-05-29 10:49:52 +0100

    feat: added initial job status class boilerplate

---
+++ src/survey_assist_themes/utils/job_status.py
@@ -0,0 +1,8 @@
+"""Handler for recording job statuses in a firestore db."""
+
+
+class JobStatus:
+    """Connect to a firestore db and create/update the status of a job."""
+
+    def __init__(self):
+        pass
