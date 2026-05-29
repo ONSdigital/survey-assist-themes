49fedc5325602c4c6790e91d4ca49cbd224ac886
Ethan Moss <ethan.moss@ons.gov.uk>
2026-05-29 15:33:42 +0100

    feat: added state enum; refactor: single update method

--- src/survey_assist_themes/utils/job_status.py
+++ src/survey_assist_themes/utils/job_status.py
@@ -1,10 +1,42 @@
 """Handler for recording job statuses in a firestore db."""
 
 import datetime
+from dataclasses import dataclass
+from enum import Enum
 
 from firebase_admin import firestore, get_app, initialize_app
 from google.api_core.exceptions import ServiceUnavailable
 from google.api_core.retry import Retry
+
+
+@dataclass(frozen=True)
+class _JobStateDetails:
+    """Details properties for job state."""
+
+    msg: str
+    start: bool = False
+    end: bool = False
+
+
+class JobState(Enum):
+    """Possible states for a job, with associated messages."""
+
+    STARTED = _JobStateDetails("Job has started", start=True)
+    IN_PROGRESS = _JobStateDetails("Job is in progress")
+    COMPLETED = _JobStateDetails("Job is completed", end=True)
+    FAILED = _JobStateDetails("Job has failed", end=True)
+
+    @property
+    def msg(self) -> str:
+        return self.value.msg
+
+    @property
+    def start(self) -> bool:
+        return self.value.start
+
+    @property
+    def end(self) -> bool:
+        return self.value.end
 
 
 class JobStatus:
@@ -62,8 +94,7 @@ class JobStatus:
     @staticmethod
     def _job_status_to_dict(
         user_id: str,
-        status: str,
-        msg: str = "",
+        state: JobState,
         start_time: datetime.datetime = None,
         end_time: datetime.datetime = None,
     ):
@@ -72,8 +103,8 @@ class JobStatus:
         job_status = {
             "user_id": user_id,
             "updated_time": now,
-            "status": status,
-            "msg": msg,
+            "state": state.name,
+            "msg": state.msg,
         }
         if start_time is not None:
             job_status["start_time"] = start_time
@@ -81,30 +112,55 @@ class JobStatus:
             job_status["end_time"] = end_time
         return job_status
 
-    def create(self, status: str, msg: str = ""):
-        """Create a new job status document in the firestore db."""
+    def _status_exists(self, job_id) -> bool:
+        """Check if a job status document already exists in the db."""
+        doc_ref = self._col_ref.document(job_id)
+        doc = doc_ref.get(retry=self._retry)
+        return doc.exists
+
+    def update(self, state: JobState):
+        """Update a job status document in the firestore db.
+
+        Parameters
+        ----------
+        state : JobState
+            The state to update the job status to.
+
+        Raises
+        ------
+        ValueError
+            - If JobState.STARTING is used and a job status document already
+            exists (status updates are unique per job).
+            - If a non-JobState.STARTING state is used but no job status
+            document exists (an initial status update is missing, and this
+            would result in missing field information e.g. start_time).
+        """
         doc_ref = self._col_ref.document(self._job_id)
-        start_time = datetime.datetime.now(tz=datetime.UTC)
+        now = datetime.datetime.now(tz=datetime.UTC)
+
+        # these cases handle unintentional misuse of state in upstream logic
+        # case start state but status already exists
+        if state.start and self._status_exists(self._job_id):
+            raise ValueError(
+                f"Job status for {self._job_id} already exists but the "
+                f"provided state is {state.name}. Expected no existing status "
+                "at the job start stage."
+            )
+        # case non-start state and no existing status
+        elif not state.start and not self._status_exists(self._job_id):
+            raise ValueError(
+                f"No existing job status for {self._job_id} but the provided "
+                f"state is {state.name}. Expected an existing status if the "
+                f" state is not {JobState.STARTED.name}."
+            )
+
         doc_ref.set(
             self._job_status_to_dict(
                 user_id=self._user_id,
-                status=status,
-                msg=msg,
-                start_time=start_time,
+                state=state,
+                start_time=now if state.start else None,
+                end_time=now if state.end else None,
             ),
-            retry=self._retry,
-        )
-
-    def update(self, status: str, msg: str = "", job_end: bool = False):
-        """Update an existing job status document in the firestore db."""
-        doc_ref = self._col_ref.document(self._job_id)
-        end_time = datetime.datetime.now(tz=datetime.UTC) if job_end else None
-        doc_ref.update(
-            self._job_status_to_dict(
-                user_id=self._user_id,
-                status=status,
-                msg=msg,
-                end_time=end_time,
-            ),
+            merge=True,
             retry=self._retry,
         )
